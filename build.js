37340495a045ce234272cae790badd2fefb61669
Neal Shah <30693865+ProgrammerIn-wonderland@users.noreply.github.com>
2025-04-05 20:31:04 -0400

    honestly not a fan of vercel

--- build.js
+++ build.js
@@ -8,11 +8,13 @@ async function setup() {
         startServer();
     } catch(e) {
         console.log("Anura static not installed, downloading")
-        await fs.writeFile("static.zip", Buffer.from(await (await (fetch("https://nightly.link/MercuryWorkshop/anuraOS/workflows/build.yaml/main/Anura%20static%20build.zip"))).arrayBuffer())) // Insanity
-        console.log("Extracting to " + __dirname + "/")
+        await fs.writeFile("static.zip", Buffer.from(await (await (fetch("https://nightly.link/MercuryWorkshop/anuraOS/workflows/build.yaml/main/Anura%20static%20build.zip"))).arrayBuffer())); // Insanity
         
-        await extract("static.zip", { dir: __dirname + "/" });
-        await extract("anura.zip", { dir: __dirname + "/" });
+        await fs.mkdir("static");
+        console.log("Extracting to " + __dirname + "/static")
+        
+        await extract("static.zip", { dir: __dirname + "/static" });
+        await extract("anura.zip", { dir: __dirname + "/static" });
         startServer();
     }
 }
