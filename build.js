402c88acdb1a3af5ab1f04fbb64785ab3b78b464
Neal Shah <30693865+ProgrammerIn-wonderland@users.noreply.github.com>
2025-04-05 21:16:41 -0400

    okay that one was my bad

--- build.js
+++ build.js
@@ -8,17 +8,14 @@ async function setup() {
     } catch(e) {
         console.log("Anura static not installed, downloading")
         await fs.writeFile("static.zip", Buffer.from(await (await (fetch("https://github.com/MercuryWorkshop/anuraOS/releases/download/latest/anura.zip"))).arrayBuffer())); // Insanity
-        
         await fs.mkdir("static");
-        console.log(await fs.readdir(__dirname + "/static"))
+        console.log("Extracting to " + __dirname + "/static")
+        await extract("static.zip", { dir: __dirname + "/static" });
+        
         const config = JSON.parse(await fs.readFile(__dirname + "/static/config.json", "utf-8"));
         // config chanegs
         config.defaultsettings["wisp-url"] = "wss://anura.pro/"; // Wisp server in lieu of vercel supporting one
         fs.writeFile(__dirname + "/static/config.json", JSON.stringify(config));
-
-        console.log("Extracting to " + __dirname + "/static")
-        
-        await extract("static.zip", { dir: __dirname + "/static" });
     }
 }
 setup();
